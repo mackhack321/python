606347965f1897f3f2d01872ed39b08dddc399dc
Mack Stanley <mlgprohacker@gmail.com>
2017-12-19 13:35:27 -0600

    did midterm digit program in python

---
+++ greeneMidtermDigitsPY.py
@@ -0,0 +1,7 @@
+# This program was originally written in C#
+# This is the Python adaptation
+
+num = input("Give a number to turn into a list of digits: ")
+numList = list(num)
+print(f"Here ya go: {numList}")
+input()
