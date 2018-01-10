09ed283ee6649b1b570518d10c4843e5d1e700cd
Mack Stanley <mlgprohacker@gmail.com>
2018-01-10 13:00:13 -0600

    more rect work

--- greeneRectCut.py
+++ greeneRectCut.py
@@ -1,10 +1,20 @@
-def cutit(h,w):
-    dirs = ((1,0),(-1,0),(0,-1),(0,1))
-    if h & 1: h,w = w,h
-    if h & 1: return 0
-    if w == 1: return 1
-    count = 0
-
-    next = [w+1, -w-1, -1, 1]
-    blen = (h+1) * (w+1) - 1
-    grid = [False] * (blen + 1)
+#the end of a python range is 1 less than the given value. range(3) = 0,1,2
+from random import randint
+possibleRights = width = 4
+possibleDowns = height = 3
+ls = ["r","r","r","r","r","r","r","r","r","r"]
+answers = []
+while len(answers) != 9:
+    for i in range(possibleDowns):
+        rand = randint(0,len(ls) - 1)
+        if ls[rand] != "d":
+            ls[rand] = "d"
+        else:
+            i = i - 1
+            continue
+        if ls not in answers:
+            answers.append(ls)
+        else:
+            continue
+        print(ls)
+print(answers)
