928f4e6f7360db5db50078f8e0d28df3978c9ee4
Mack Stanley <mlgprohacker@gmail.com>
2018-01-24 10:02:53 -0600

    did webscraping project

---
+++ webscraper.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen
+
+url = "https://www.newegg.com/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=7709"
+client = urlopen(url)
+html = client.read()
+client.close()
+
+urlsoup = soup(html, "html.parser")
+f = open("data.csv","w")
+f.write("Brand,Name,Shipping,Price\n")
+
+containers = urlsoup.findAll("div",{"class":"item-container"})
+for container in containers:
+    productName = container.a.img["title"].replace(","," |")
+    try:
+        brandName = container.div.div.a.img["title"]
+    except AttributeError:
+        brandName = "NA"
+
+    itemaction = container.find("div",{"class":"item-action"})
+    try:
+        dollars = itemaction.ul.find("li",{"class":"price-current"}).strong.text
+        cents = itemaction.ul.find("li",{"class":"price-current"}).sup.text
+        price = dollars.replace(",",".") + cents
+    except AttributeError:
+        price = "NA"
+    shipping = itemaction.ul.find("li",{"class":"price-ship"}).text.strip()
+    f.write(f"{brandName},{productName},{shipping},{price}\n")
+f.close()
+print("Done!")
