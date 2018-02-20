70c5d8c4b2167f9e2468e35099cb039b984dae8d
Mack Stanley <mlgprohacker@gmail.com>
2018-02-20 17:02:09 -0600

    lots of work on transposition

--- transposition.py
+++ transposition.py
@@ -1,23 +1,15 @@
-# while counter < numOfChars:
-#     for i in range(counter,key):
-#         print(counter)
-#         rowls.append(usrls[i])
-#         counter += 1
-#     bigls.append(rowls)
-#     print(rowls)
-
-# print(usrls[0:key]) # ['t', 'h', 'e', ' ', 'q']
-# print(usrls[0+key:key+key]) # ['u', 'i', 'c', 'k', ' ']
-# print(usrls[0+key+key:key+key+key]) # ['b', 'r', 'o', 'w', 'n']
+# "the quick brown fox" with key of 5: tub hirfecoo kwxq n|
+# "pure imagination" with key of 5: pinnuma|rat|egi| io|
 from math import ceil # this is needed to round up
 def encrypt(usr, key):
     usrls = list(usr)
-    bigls = []
     rows = ceil(len(usr)/key)
     numOfChars = len(usrls)
+    counter = 0
+
+    bigls = []
     rowls = []
     encryptedls = []
-    counter = 0
 
     while counter != rows:
         newls = usrls[0+(key*counter):key+(key*counter)]
@@ -29,26 +21,26 @@ def encrypt(usr, key):
     for num in range(key):
         encryptedls.append("".join(i[num] for i in bigls))
 
+    for i in bigls:
+        print(i)
     return encryptedls
 
-def decrypt(encrypted,key):
+def decrypt(encrypted, key):
+    encryptedls = list(encrypted)
+    rows = rows = ceil(len(usr)/key)
     decryptedls = []
-    counter = 0
-    while counter != len(bigls):
-        for ls in bigls:
-            decryptedls.append(ls[0])
-            counter += 1
-    print(decryptedls)
+    for i in range(0,rows):
+        for ls in encryptedls[i::4]:
+            decryptedls.append(ls)
+        decryptedls = [i for i in decryptedls if i != "|"]
+    return decryptedls
 
-def toList(msg,key):
-    bigls = []
-    newls = []
-    rows = ceil(len(msg)/key)
-    while counter != rows:
-        newls = usrls[0+(key*counter):key+(key*counter)]
-        bigls.append(newls)
-    return(bigls)
-
-usr = input("Message to encrypt: ")
-key = int(input("Key: "))
-print("".join(encrypt(usr,key)))
+choice = input("Encrypt or decrypt? e/d: ")
+if choice == "e":
+    usr = input("Message to encrypt: ")
+    key = int(input("Key: "))
+    print("".join(encrypt(usr,key)))
+if choice == "d":
+    usr = input("Message to decrypt: ")
+    key = int(input("Key: "))
+    print("".join(decrypt(usr, key)))
