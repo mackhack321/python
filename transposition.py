fd19dbae8e1a4c53ce1314d2480fffa8a8b0d5fb
Mack Stanley <mlgprohacker@gmail.com>
2018-02-21 09:19:21 -0600

    nailed down decryption, added debugging with argv

--- transposition.py
+++ transposition.py
@@ -1,6 +1,20 @@
 # "the quick brown fox" with key of 5: tub hirfecoo kwxq n|
 # "pure imagination" with key of 5: pinnuma|rat|egi| io|
+# "Mack has found out something neat: Transposition encryption!" with key of 4:
+# M  nuoh trpt riahfdtmin:aoieyocao  ene nsonpnksuostgaTsinct!
+# "mack is the best" with key of 6: msba ectskht e|i |
+
+# run the program with --showArray to see the encryption grid
 from math import ceil # this is needed to round up
+from sys import argv # this lets us see the command line args
+
+def showArrayIfArg(bigls): # this takes in a raw list and, if the arg is present, displays each item in the list
+     try:
+         if "--showArray" in argv:
+             for i in bigls:
+                 print(i)
+     except: pass
+
 def encrypt(usr, key):
     usrls = list(usr)
     rows = ceil(len(usr)/key)
@@ -21,19 +35,19 @@ def encrypt(usr, key):
     for num in range(key):
         encryptedls.append("".join(i[num] for i in bigls))
 
-    for i in bigls:
-        print(i)
-    return encryptedls
+    showArrayIfArg(bigls) # debug
+    return encryptedls # this function returns a raw list!!! it is up to the frontend to make it pretty
 
 def decrypt(encrypted, key):
     encryptedls = list(encrypted)
-    rows = rows = ceil(len(usr)/key)
+    rows = ceil(len(usr)/key)
     decryptedls = []
     for i in range(0,rows):
-        for ls in encryptedls[i::4]:
+        for ls in encryptedls[i::rows]: # need to find the right splice
             decryptedls.append(ls)
+            showArrayIfArg(ls) # debug
         decryptedls = [i for i in decryptedls if i != "|"]
-    return decryptedls
+    return decryptedls # this function returns a raw list!!! it is up to the frontend to make it pretty
 
 choice = input("Encrypt or decrypt? e/d: ")
 if choice == "e":
