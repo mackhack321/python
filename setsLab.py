41e64b2ca4814d08811c90a6c0f46a29e3ee04bb
Mack Stanley <mlgprohacker@gmail.com>
2018-03-14 10:30:56 -0500

    work on sets lab

--- setsLab.py
+++ setsLab.py
@@ -41,11 +41,30 @@ def dealHand(amt): # takes int for amt and returns hand as a list
         hand.append(randcard)
     return hand
 
+def allSame(ls):
+    for item in ls:
+        if ls[0] == item: same = True
+        else:
+            same = False
+            break
+    return same
+
 def checkHand(hand):
+    # Royal flush – 10, Jack, Queen, King, Ace all same suit
+    # Straight flush – five ranks in a row, all same suit
+    # Four of a kind – Four of same rank
+    # Full house – three of one rank and two of another
+    # Flush – five of same suit
+    # Straight – five ranks in row
+    # Three of a kind – three of one rank (not part of four or full house)
+    # Two pair – two each of two ranks
+    # Pair – two of same rank (not part of any other hand)
+    # X High – If none above fit, X is the highest rank in hand.
     ranks = [deck[i]["rank"] for i in hand]
     suits = [deck[i]["suit"] for i in hand]
-    value = [deck[i]["value"] for i in hand]
-    print(ranks,suits,value)
+    values = [deck[i]["value"] for i in hand]
+    if "10" in values and "J" in values and "Q" in values and "K" in values and "A" in values and allSame(suits):
+        royalFlush = True
 
 deck = buildDeck()
 hand = dealHand(amt=5)
