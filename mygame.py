500bb79bdbebe3e9578cc028b25724955125d71c
Mack Stanley <mlgprohacker@gmail.com>
2018-04-03 08:59:34 -0500

    work on game

--- mygame.py
+++ mygame.py
@@ -2,14 +2,17 @@ import pygame as pg
 from nicecolors import purple
 
 pg.init()
-pg.display.set_caption("My Game")
-screen = pg.display.set_mode((500,500))
-screen.fill(purple)
-pg.display.update()
+pg.display.set_caption("My Game") # window caption
+screen = pg.display.set_mode((500,500)) # resolution
+screen.fill(purple) # bg color
+pg.display.update() # apply color change
 
-#pg.rect.Rect(screen, pg.color.Color("black"), (200,200,100,100),0)
-rectangle = pg.rect.Rect(200,200,100,100)
-pg.draw.rect(rectangle)
+myfont = pg.font.SysFont("Comic Sans MS",30)
+text = myfont.render("Hello, World!",True,pg.Color("black"))
+textrect = text.get_rect()
+textrect.centerx = screen.get_rect().centerx
+textrect.centery = screen.get_rect().centery
+screen.blit(text, textrect)
 pg.display.update()
 
 running = True
@@ -17,5 +20,3 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-        if event.type == pg.MOUSEBUTTONDOWN:
-            print("mouse down")
