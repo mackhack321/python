0d3d4be040fd0211416205463b5c2651b8e639ec
Mack Stanley <mlgprohacker@gmail.com>
2018-04-05 17:20:49 -0500

    work on game, clean data folder

--- Clicker/game.py
+++ Clicker/game.py
@@ -17,12 +17,13 @@ class Player():
         self.mult = 1
         self.score = 0
         self.candrag = False
+        self.maxscore = False
     def setMult(self, newmult):
         self.mult = newmult
     def setScore(self, newscore):
         self.score = newscore
     def addPoints(self, amt):
-        self.score += amt
+        if not self.maxscore: self.score += amt
     def subPoints(self,amt):
         self.score -= amt
     def loadData(self, filename):
@@ -34,6 +35,8 @@ class Player():
     def saveData(self,filename):
         pkl.dump({"score":self.score, "mult":self.mult}, open(filename,"wb"))
     def reset(self):
+        resetChannel.play(resetsound,0)
+        self.maxscore = False
         self.mult = 1
         self.score = 0
 
@@ -65,36 +68,41 @@ class Upgrade:
         self.cost = cost
         self.multupgrade = multupgrade
     def buy(self):
-        if player.score >= self.cost:
-            player.setMult(player.mult+self.multupgrade)
-            player.subPoints(self.cost)
-            myfont = pg.font.SysFont("Comic Sans MS",30)
-            text = myfont.render(f"Bought {self.name}!",True,WHITE)
-            screen.blit(text,(350,500))
-            pg.display.update()
-            sleep(1)
+        if not player.maxscore:
+            if player.score >= self.cost:
+                player.setMult(player.mult+self.multupgrade)
+                player.subPoints(self.cost)
+                playSound("buysound")
+                myfont = pg.font.SysFont("Comic Sans MS",30)
+                text = myfont.render(f"Bought {self.name}!",True,WHITE)
+                screen.blit(text,(350,500))
+                pg.display.update()
+                sleep(1)
 
 def doClick(mult):
-    pos = pg.mouse.get_pos()
-    if keyboard.rect.collidepoint(pos) == 1:
-        playSound("keypress")
-        player.addPoints(mult)
-        changeMonitorImage()
-    if newkeeb.rect.collidepoint(pos) == 1:
-        if player.candrag:
-            newkeeb.repos(pg.mouse.get_pos()[0],pg.mouse.get_pos()[1])
-            print("New Keeb: ",newkeeb.currentx,newkeeb.currenty)
-        newkeeb_obj.buy()
-    if newide.rect.collidepoint(pos) == 1:
-        if player.candrag:
-            newide.repos(pg.mouse.get_pos()[0],pg.mouse.get_pos()[1])
-            print("New IDE: ",newide.currentx,newide.currenty)
-        newide_obj.buy()
-    if topkek.rect.collidepoint(pos) == 1:
-        if player.candrag:
-            topkek.repos(pg.mouse.get_pos()[0],pg.mouse.get_pos()[1])
-            print("Topkek: ",topkek.currentx,topkek.currenty)
-        topkek_obj.buy()
+    if pg.mouse.get_pressed()[0]:
+        pos = pg.mouse.get_pos()
+        if keyboard.rect.collidepoint(pos) == 1:
+            playSound("keypress")
+            player.addPoints(mult)
+            changeMonitorImage()
+        if newkeeb.rect.collidepoint(pos) == 1:
+            if player.candrag:
+                newkeeb.repos(pg.mouse.get_pos()[0],pg.mouse.get_pos()[1])
+                print("New Keeb: ",newkeeb.currentx,newkeeb.currenty)
+            newkeeb_obj.buy()
+        if newide.rect.collidepoint(pos) == 1:
+            if player.candrag:
+                newide.repos(pg.mouse.get_pos()[0],pg.mouse.get_pos()[1])
+                print("New IDE: ",newide.currentx,newide.currenty)
+            newide_obj.buy()
+        if topkek.rect.collidepoint(pos) == 1:
+            if player.candrag:
+                topkek.repos(pg.mouse.get_pos()[0],pg.mouse.get_pos()[1])
+                print("Topkek: ",topkek.currentx,topkek.currenty)
+            topkek_obj.buy()
+        if reset.rect.collidepoint(pos) == 1:
+            player.reset()
 
 def drawAll():
     background.draw()
@@ -105,6 +113,7 @@ def drawAll():
     topkek.draw()
     upgradebanner.draw()
     instructions.draw()
+    reset.draw()
     guido.draw() # this must be last to go over everything else
 
 def playSound(sound):
@@ -115,22 +124,24 @@ def playSound(sound):
     elif sound == "bgmusic":
         musicChannel.play(bgmusic, -1)
         musicChannel.set_volume(.25)
+    elif sound == "buysound":
+        buysoundChannel.play(buysound, 0)
 
 def changeMonitorImage():
-    images = ["data/monitor01.png","data/monitor02.png","data/monitor03.png","data/monitor04.png","data/monitor05.png","data/monitor06.png","data/monitor07.png"]
+    images = ["data/monitor01.png","data/monitor02.png","data/monitor03.png","data/monitor04.png","data/monitor05.png","data/monitor06.png","data/monitor07.png","data/monitorbsod.png"]
     monitor.reImage(choice(images))
 
 def displayPoints(score):
     drawAll()
     myfont = pg.font.SysFont("Impact",30)
-    text = myfont.render(f"Name: {player.name}, Score: {player.score}, Multiplier: {player.mult}",True,WHITE)
+    text = myfont.render(f"Name: {player.name}, Lines of Code: {player.score}, Multiplier: {player.mult}",True,WHITE)
     screen.blit(text,(250,0))
     pg.display.update()
 
 def doQuitSequence():
-    #musicChannel.fadeout(500)
-    #playSound("winshutdown")
-    #sleep(2)
+    musicChannel.fadeout(500)
+    playSound("winshutdown")
+    sleep(2)
     player.saveData(f"players/{player.name}.pkl")
     pg.quit()
     exit()
@@ -177,6 +188,11 @@ instructions.resize(966,163)
 instructions.repos(500,960)
 instructions.draw()
 
+reset = Sprite("data/reset.png")
+reset.resize(100,100)
+reset.repos(75,300)
+reset.draw()
+
 guido = Sprite("data/rossum.png")
 guido.resize(70,105)
 guido.draw()
@@ -184,17 +200,25 @@ guido.draw()
 keysound = pg.mixer.Sound("data/keypress.wav")
 winshutdown = pg.mixer.Sound("data/winshutdown.wav")
 bgmusic = pg.mixer.Sound("data/music.wav")
+buysound = pg.mixer.Sound("data/buysound.wav")
+resetsound = pg.mixer.Sound("data/resetsound.wav")
 musicChannel = pg.mixer.Channel(0)
 keyChannel = pg.mixer.Channel(1)
 shutdownChannel = pg.mixer.Channel(2)
+buysoundChannel = pg.mixer.Channel(3)
+resetChannel = pg.mixer.Channel(4)
 playSound("bgmusic")
 ### Make and get player data ###
-player = Player("Mack") # EDIT THIS ARGUMENT TO CHANGE PLAYER PROFILE!!!!!
+player = Player("debug") # EDIT THIS ARGUMENT TO CHANGE PLAYER PROFILE!!!!!
 if player.name == "debug": player.candrag = False # CHANGE TO TRUE TO LET debug DRAG UPGRADES
 player.loadData(f"players/{player.name}.pkl")
 ### Game loop ###
 running = True
 while running:
+    if not player.maxscore:
+        if player.score > 10000:
+            player.maxscore = True
+            player.setScore("Way too many")
     displayPoints(player.score)
     guido.repos(pg.mouse.get_pos()[0],pg.mouse.get_pos()[1])
     pg.display.update()
