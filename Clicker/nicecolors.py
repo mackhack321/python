adc96c4d16bd6a0fd3b44e51099e654d1b2fda29
Mack Stanley <mlgprohacker@gmail.com>
2018-04-06 10:57:07 -0500

    add __main__ message to nicecolors

--- Clicker/nicecolors.py
+++ Clicker/nicecolors.py
@@ -2,3 +2,10 @@ PURPLE = (115, 2, 127)
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 YELLOW = (255,255,0)
+
+if __name__ == "__main__":
+    print("Hello there!  This is a library used in Software Developer Simulator 1989.")
+    print("This library holds some constants for common colors, some of which are used in SDS.")
+    print("Running this library as it is will do nothing but show you this, so why not play the game?")
+    print("Run 'python game.py YOURNAME' to start a new game!")
+    input("Press enter to exit...")
